ffbe313046afb167a367b8ba300377ae78b14d68
Star Bist <malimirkeccita@gmail.com>
2017-02-27 09:23:15 +0100

    Updating gulp tasks.

--- gulp/minify.js
+++ gulp/minify.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
-gulp.task('minify', function() {
-  return gulp.src('./*.css')
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./'));
+gulp.task('css:minify', function() {
+  return gulp.src('./dev/css/**/*.css')
+  .pipe(cleanCSS())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('./dist/css/'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,6 @@ var gulp = require('gulp');
 var requireDir = require('require-dir');
 requireDir('./gulp');
 
-gulp.task('dev', ['sequence-dev']);
+gulp.task('default:dev', ['sequence:dev']);
 
-gulp.task('dev-watch', ['sequence-dev'], function() {
-  gulp.watch('./sass/**/*.scss', ['sequence-dev']);
-});
-
-gulp.task('dist', ['sequence-dist']);
-
-gulp.task('dist-watch', ['sequence-dist'], function() {
-  gulp.watch('./sass/**/*.scss', ['sequence-dist']);
-});
+gulp.task('default:dist', ['sequence:dist']);
