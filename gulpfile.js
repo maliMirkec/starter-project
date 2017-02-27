0ef3bd2bf8fcb192e9b9bd2814470ff88f99b036
Star Bist <malimirkeccita@gmail.com>
2017-02-27 09:36:45 +0100

    Fixing gulp watch.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,26 @@
 var gulp = require('gulp');
 
 var requireDir = require('require-dir');
+var gulpSequence = require('gulp-sequence');
+
 requireDir('./gulp');
 
 gulp.task('default:dev', ['sequence:dev']);
 
+gulp.task('watch:dev', function() {
+  gulp.watch('./src/css/**/*.scss', function() {
+    gulpSequence('css:sass', 'css:critical')(function (err) {
+      if (err) console.log(err)
+    });
+  });
+});
+
 gulp.task('default:dist', ['sequence:dist']);
+
+gulp.task('watch:dist', function() {
+  gulp.watch('./src/css/**/*.scss', function() {
+    gulpSequence('css:sass', 'css:critical', 'css:autoprefix', 'css:minify')(function (err) {
+      if (err) console.log(err)
+    });
+  });
+});
