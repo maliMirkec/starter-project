ca92ed96b59ca5c4a24658fe4f7f2742144f5dc9
Star Bist <malimirkeccita@gmail.com>
2017-10-29 17:30:49 +0100

    Removing dev folder. Adding gulp tasks.

--- gulp/minify.js
+++ gulp/minify.js
@@ -2,13 +2,19 @@
 
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
 
 gulp.task('css:minify', function() {
   return gulp.src('./dev/css/**/*.css')
   .pipe(cleanCSS())
-  .pipe(rename({
-    suffix: '.min'
-  }))
   .pipe(gulp.dest('./dist/css/'));
+});
+
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('html:minify', function() {
+  return gulp.src('./src/html/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./dist/'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -10,23 +10,13 @@ requireDir('./gulp');
 gulp.task('default:dev', ['sequence:dev']);
 
 gulp.task('watch:dev', function() {
-  gulp.watch('./src/css/**/*.scss', function() {
-    gulpSequence('css:sass', 'css:critical')(function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  });
+  gulp.watch('./src/css/**/*.scss', ['sequence:dev']);
 });
 
 gulp.task('default:dist', ['sequence:dist']);
 
 gulp.task('watch:dist', function() {
-  gulp.watch('./src/css/**/*.scss', function() {
-    gulpSequence('css:sass', 'css:critical', 'css:autoprefix', 'css:minify')(function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  });
+  gulp.watch('./src/css/**/*.scss', ['sequence:dist']);
 });
+
+gulp.task('default', ['html:minify', 'sequence:dist'])
