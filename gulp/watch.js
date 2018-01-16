ddd78b29cfe4bb7900ac5d2b0c84bbb40cb35f28
Star Bist <malimirkeccita@gmail.com>
2018-01-16 09:30:49 +0100

    Adding project structure. Updating theme. Updating install scripts.

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,14 +1,19 @@
 const gulp = require('gulp')
 
 gulp.task('watch:dist', () => {
-  gulp.watch(`${global.config.root + global.config.src + global.config.js.src}**/*.js`, ['jsdoc', 'js'])
-  gulp.watch(`${global.config.root + global.config.src + global.config.css.src}**/*.scss`, ['sassdoc', 'kss', 'css'])
+  gulp.watch(`${global.config.root + global.config.src + global.config.js.src}**/*.js`, ['js'])
+  gulp.watch(`${global.config.root + global.config.src + global.config.css.src}**/*.scss`, ['css'])
   gulp.watch(`${global.config.root + global.config.src + global.config.html.src}**/*.pug`, ['html:dist'])
   gulp.watch(`${global.config.root + global.config.dest + global.config.css.src}style.critical.min.css`, ['html:dist'])
 })
 
 gulp.task('watch:dev', () => {
-  gulp.watch(`${global.config.root + global.config.src + global.config.js.src}**/*.js`, ['jsdoc', 'js'])
-  gulp.watch(`${global.config.root + global.config.src + global.config.css.src}**/*.scss`, ['sassdoc', 'kss', 'css'])
+  gulp.watch(`${global.config.root + global.config.src + global.config.js.src}**/*.js`, ['js'])
+  gulp.watch(`${global.config.root + global.config.src + global.config.css.src}**/*.scss`, ['css'])
   gulp.watch(`${global.config.root + global.config.src + global.config.html.src}**/*.pug`, ['html:dev'])
+})
+
+gulp.task('watch:docs', () => {
+  gulp.watch(`${global.config.root + global.config.src + global.config.js.src}**/*.js`, ['js', 'jsdoc'])
+  gulp.watch(`${global.config.root + global.config.src + global.config.css.src}**/*.scss`, ['css', 'sassdoc', 'kss'])
 })
