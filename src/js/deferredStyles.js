cce73a1d756a7978703be2469956b57a94439de2
Star Bist <malimirkeccita@gmail.com>
2018-01-30 12:44:01 +0100

    Package 1.1.41 - Fixing build.

--- src/js/deferredStyles.js
+++ src/js/deferredStyles.js
@@ -1,7 +1,7 @@
 /**
  * A better CSS Loading.
  *
- * @module Deferred Styles
+ * @module DeferredStyles
  * @author Google Developers
  * @see https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery
  */
