0e5e85e4f0e2765b6dae06ed95c77c69d3677993
Star Bist <malimirkeccita@gmail.com>
2018-01-30 11:56:57 +0100

    Updating postinstall script.

--- postinstall.js
+++ postinstall.js
@@ -1,42 +1,61 @@
-const pathExists = require('path-exists')
-const ncp = require('ncp')
+// const pathExists = require('path-exists')
+// const ncp = require('ncp')
 
-ncp.limit = 16
+// ncp.limit = 16
 
-const configCheck1 = '../../config.json'
-const configCheck2 = '../../gulpfile.js'
-const configSrc = 'copy'
-const configDest = '../../'
+// const configCheck1 = '../../config.json'
+// const configCheck2 = '../../gulpfile.js'
+// const configSrc = 'copy'
+// const configDest = '../../'
 
-pathExists(configCheck1).then((exists1) => {
-  if (!exists1) {
-    pathExists(configCheck2).then((exists2) => {
-      if (!exists2) {
-        ncp(configSrc, configDest, (err) => {
-          if (err) {
-            return false
-          }
-        })
-      }
-    })
-  }
-})
+// pathExists(configCheck1).then((exists1) => {
+//   if (!exists1) {
+//     pathExists(configCheck2).then((exists2) => {
+//       if (!exists2) {
+//         ncp(configSrc, configDest, (err) => {
+//           if (err) {
+//             return false
+//           }
+//         })
+//       }
+//     })
+//   }
 
-const srcCheck1 = '../../new_src'
-const srcCheck2 = '../../src'
-const srcSrc = 'src'
-const srcDest = '../../new_src/'
+//   return true
+// })
 
-pathExists(srcCheck1).then((exists1) => {
-  if (!exists1) {
-    pathExists(srcCheck2).then((exists2) => {
-      if (!exists2) {
-        ncp(srcSrc, srcDest, (err) => {
-          if (err) {
-            return false
-          }
-        })
-      }
-    })
-  }
-})
+// const srcCheck1 = '../../new_src'
+// const srcCheck2 = '../../src'
+// const srcSrc = 'src'
+// const srcDest = '../../new_src/'
+
+// pathExists(srcCheck1).then((exists1) => {
+//   if (!exists1) {
+//     pathExists(srcCheck2).then((exists2) => {
+//       if (!exists2) {
+//         ncp(srcSrc, srcDest, (err) => {
+//           if (err) {
+//             return false
+//           }
+//         })
+//       }
+//     })
+//   }
+
+//   return true
+// })
+
+// const tasksSrc = 'gulp'
+// const tasksDest = '../../gulp/'
+
+// ncp(tasksSrc, tasksDest, (err) => {
+//   if (err) {
+//     return false
+//   }
+
+//   return true
+// })
+
+console.log(process.env)
+console.log(process.argv)
+console.log(process.argv.npm_package_config_first)
