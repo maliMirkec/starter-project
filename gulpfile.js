84ada535c888a248cf6ea73dda899beff08f7fb3
Star Bist <malimirkeccita@gmail.com>
2018-02-04 11:22:48 +0100

    Fixing kill process.

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,18 @@ global.bs = require('browser-sync').create()
 
 requireDir(global.config.root + global.config.gulp.src)
 
-gulp.task('dev:sequence', (callback) => {
-  gulpSequence('clean', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dev' : '', 'watch:dev', global.config.penthouse.run ? 'critical:dev' : '', global.config.kill.dev.run ? 'kill:delay' : '')(callback)
-})
+gulp.task('dev:sequence', callback => gulpSequence('clean', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dev' : '', 'watch:dev', 'browser:sync', global.config.penthouse.run ? 'critical:dev' : '', global.config.kill.dev.run ? 'kill:delay' : '')(callback))
 
 gulp.task('dev', ['dev:sequence'])
 
-gulp.task('docs:sequence', (callback) => {
-  gulpSequence('clean', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dev' : '', 'watch:docs', 'browser:sync', global.config.kill.docs.run ? 'kill:delay' : '')(callback)
-})
+gulp.task('docs:sequence', callback => gulpSequence('clean', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dev' : '', 'watch:docs', 'browser:sync', global.config.kill.docs.run ? 'kill:delay' : '')(callback))
 
 gulp.task('docs', ['docs:sequence'])
 
-gulp.task('dist:sequence', (callback) => {
-  gulpSequence('clean', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dist' : '', 'watch:dist', global.config.penthouse.run ? 'critical:dist' : '', global.config.kill.dist.run ? 'kill:delay' : '')(callback)
-})
+gulp.task('dist:sequence', callback => gulpSequence('clean', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dist' : '', 'watch:dist', 'browser:sync', global.config.penthouse.run ? 'critical:dist' : '', global.config.kill.dist.run ? 'kill:delay' : '')(callback))
 
 gulp.task('dist', ['dist:sequence'])
 
-gulp.task('deploy:sequence', (callback) => {
-  gulpSequence('clean', global.config.favicon.run ? 'favicon' : '', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js:deploy' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css:deploy' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dist' : '', 'watch:dist', global.config.penthouse.run ? 'critical:deploy' : '', global.config.gzip.run ? 'gzip' : '', global.config.kill.deploy.run ? 'kill:delay' : '')(callback)
-})
+gulp.task('deploy:sequence', callback => gulpSequence('clean', global.config.favicon.run ? 'favicon' : '', global.config.gfx.run ? 'gfx' : '', global.config.fonts.run ? 'fonts' : '', global.config.js.run ? 'js:deploy' : '', global.config.jsdoc.run ? 'jsdoc' : '', global.config.css.run ? 'css:deploy' : '', global.config.sassdoc.run ? 'sassdoc' : '', global.config.kss.run ? 'kss' : '', global.config.html.run ? 'html:dist' : '', 'watch:dist', 'browser:sync', global.config.penthouse.run ? 'critical:deploy' : '', global.config.gzip.run ? 'gzip' : '', global.config.kill.deploy.run ? 'kill:delay' : '')(callback))
 
 gulp.task('default', ['deploy:sequence'])
