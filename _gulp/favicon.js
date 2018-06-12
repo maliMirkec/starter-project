078e657264410d7bf92a1fe80c1d0c5dc9fb704a
Star Bist <malimirkeccita@gmail.com>
2018-06-12 08:56:40 +0200

    Updating favicon task.

--- _gulp/favicon.js
+++ _gulp/favicon.js
@@ -10,20 +10,22 @@ const runSequence = require('run-sequence')
 gulp.task('favicon:generate', done =>
   realFavicon.generateFavicon(global.config.favicon.realFaviconConfig, () => done()))
 
-const parsedFaviconFile = fs.readFileSync(global.config.favicon.realFaviconConfig.markupFile)
-const parsedFaviconData = JSON.parse(parsedFaviconFile)
+if (global.config.favicon.run) {
+  const parsedFaviconFile = fs.readFileSync(global.config.favicon.realFaviconConfig.markupFile)
+  const parsedFaviconData = JSON.parse(parsedFaviconFile)
 
-// Inject the favicon markups in your HTML pages. You should run
-// this task whenever you modify a page. You can keep this task
-// as is or refactor your existing HTML pipeline.
-gulp.task('favicon:inject', () => gulp.src([global.config.root + global.config.favicon.src])
-  .pipe(realFavicon.injectFaviconMarkups(parsedFaviconData.favicon.html_code))
-  .pipe(gulp.dest(global.config.root + global.config.favicon.dest)))
+  // Inject the favicon markups in your HTML pages. You should run
+  // this task whenever you modify a page. You can keep this task
+  // as is or refactor your existing HTML pipeline.
+  gulp.task('favicon:inject', () => gulp.src([global.config.root + global.config.favicon.src])
+    .pipe(realFavicon.injectFaviconMarkups(parsedFaviconData.favicon.html_code))
+    .pipe(gulp.dest(global.config.root + global.config.favicon.dest)))
 
-// Check for updates on RealFaviconGenerator (think: Apple has just
-// released a new Touch icon along with the latest version of iOS).
-// Run this task from time to time. Ideally, make it part of your
-// continuous integration system.
-gulp.task('favicon:update', done => realFavicon.checkForUpdates(parsedFaviconData.version))
+  // Check for updates on RealFaviconGenerator (think: Apple has just
+  // released a new Touch icon along with the latest version of iOS).
+  // Run this task from time to time. Ideally, make it part of your
+  // continuous integration system.
+  gulp.task('favicon:update', done => realFavicon.checkForUpdates(parsedFaviconData.version))
 
-gulp.task('favicon', callback => runSequence('favicon:generate', 'favicon:inject', callback))
+  gulp.task('favicon', callback => runSequence('favicon:generate', 'favicon:inject', callback))
+}
